3e89f02528bf713feb878785a5f219ecf35f1be5
Антон Тришин <anton@MacBook-Pro-Anton.local>
2022-02-23 07:40:12 -0800

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Register service worker to control making site work offline
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('sw.js')
+//     .then(() => { console.log('Service Worker Registered'); });
+// }
 
 // Code to handle install prompt on desktop
 
--- sw.js
+++ sw.js
@@ -6,17 +6,17 @@
 // });
 
 // New version serviceWorker
-
-const staticCacheName = 's-app-v1'
-const dynamicCacheName = 'd-app-v1'
-
-const assetUrls = [
-    '%PUBLIC_URL%/index.html',
-    '%PUBLIC_URL%/static',
-    '%PUBLIC_URL%/index.js'
-]
-
-self.addEventListener('install', async event => {
-  const cache = await caches.open(staticCacheName)
-  await cache.addAll(assetUrls)
-})
+//
+// const staticCacheName = 's-app-v1'
+// const dynamicCacheName = 'd-app-v1'
+//
+// const assetUrls = [
+//     '%PUBLIC_URL%/index.html',
+//     '%PUBLIC_URL%/static',
+//     '%PUBLIC_URL%/index.js'
+// ]
+//
+// self.addEventListener('install', async event => {
+//   const cache = await caches.open(staticCacheName)
+//   await cache.addAll(assetUrls)
+// })
