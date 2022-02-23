b81172570415a1a960138a9829049762bdad2e99
Антон Тришин <anton@MacBook-Pro-Anton.local>
2022-02-23 07:06:35 -0800

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
+// self.addEventListener('fetch', (e) => {
+//   console.log(e.request.url);
+//   e.respondWith(
+//     caches.match(e.request).then((response) => response || fetch(e.request)),
+//   );
+// });
+
+// New version serviceWorker
+
+const staticCacheName = 's-app-v1'
+const dynamicCacheName = 'd-app-v1'
+
+const assetUrls = [
+    '/index.html',
+    '/static',
+    '/index.js'
+]
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(assetUrls)
+})
